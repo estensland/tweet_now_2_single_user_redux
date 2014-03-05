dc506e9d28294be5a3fe5c60274d544347069a6d
Daniel Lu <dandydanny@gmail.com>
2014-03-04 23:20:23 -0600

    Changed title tag, removed debugging puts

--- app/helpers/make_client.rb
+++ app/helpers/make_client.rb
@@ -1,19 +1,5 @@
 def make_twitter_client
-  puts "Make twitter user --------------"
-  puts "session ID --------------"
-  puts session[:user_id]
-  puts "Twitter key --------------"
-  puts ENV['TWITTER_KEY']
-  puts "twitter secret --------------"
-  puts ENV['TWITTER_SECRET']
-
   u = User.find(session[:user_id])
-  puts "oauth token --------------"
-  puts u.oauth_token
-  puts "oauth secret--------------"
-  puts u.oauth_secret
-
-
   @client = Twitter::Client.new(
       consumer_key: ENV['TWITTER_KEY'],
       consumer_secret: ENV['TWITTER_SECRET'],
