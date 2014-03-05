81e5ae531e13a7553d095ead82765f11d5292ea6
Daniel Lu <dandydanny@gmail.com>
2014-03-04 22:21:53 -0600

    Cleaned up sourcecode, add notes for twitter obj

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,4 @@
 get '/' do
-  puts "user id------------------------"
-  puts session[:user_id]
   if session[:user_id]
     @user = User.find(session[:user_id])
     erb :index
@@ -23,42 +21,21 @@ get '/auth' do
   # the `request_token` method is defined in `app/helpers/oauth.rb`
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
   session[:access_token] = @access_token
-  puts "IN auth, access_token is -----------------------------------------"
-  p @access_token
-  p session[:access_token]
-  puts "SN is-----------------------------------------"
-  p @access_token.params[:screen_name]
-  # puts "IN auth, access_token is -----------------------------------------"
-  # puts "IN auth, access_token is -----------------------------------------"
-  # puts "IN auth, access_token is -----------------------------------------"
+
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
 
   # at this point in the code is where you'll need to create your user account and store the access token
 
   # grab twitter username
-  # find or create twitter username
-  #find_or_create doesn't work?
+  # find or create twitter username (Doesn't seem to work. Why?)
   # @user = User.find_by(username: @access_token.params[:screen_name]) || User.create(username: @access_token.params[:screen_name])
   # update user record with obtained access token & access token secret
-
-  puts
-
-
   @user = User.create(username: @access_token.params[:screen_name])
   @user.update_attributes( oauth_token: @access_token.params[:oauth_token], oauth_secret: @access_token.params[:oauth_token_secret])
   # set session[:user_id] with User.id (logged in)
   session[:user_id] = @user.id
   # render tweet form
-
-  # $CLIENT = Twitter::Client.new
-
-  puts "@client-------------------"
-
-  # @client = make_twitter_client()
-
-  # To do, fix refresh breakage
-  # make this actually tweet
 
   redirect to '/'
   # erb :index
@@ -66,11 +43,9 @@ get '/auth' do
 end
 
 post '/tweet' do
-  p params
-  p session[:user_id]
-  puts "@client-------------------"
   # call helper method
   make_twitter_client
+  200
   # Do API Call
-  @client.update(params["message"]).text
+  # @client.update(params["message"]).text
 end
--- public/js/application.js
+++ public/js/application.js
@@ -7,16 +7,16 @@ $(document).ready(function() {
     var sayings = $("#tweet-input").val()
     console.log(sayings);
     $( "#tweet-input" ).prop( "disabled", true);
-    $( "#tweet-input" ).animate( { backgroundColor: "yellow" });
+    $( "#tweet-input" ).animate( { borderColor: "white" });
     $.ajax({
       type: "POST",
       url: "/tweet",
       data: { message: sayings }
     }).done(function(msg) {
-      $( "#tweet-input" ).animate( { backgroundColor: "#7CFC00" });
+      $( "#tweet-input" ).animate( { borderColor: "#00FF00" });
       $( "#tweet-input" ).prop( "disabled", false );
     }).fail(function(msg) {
-      $( "#tweet-input" ).animate( { backgroundColor: "#DB7093" });
+      $( "#tweet-input" ).animate( { borderColor: "#FF0000" });
       $( "#tweet-input" ).prop( "disabled", false );
     });
   });
